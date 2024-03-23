d2d66472d09fd02fca543621cb94e256a2313785
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-23 21:22:51 +0800

    Add eslint plugin for consistent code style

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -6,16 +6,21 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@stylistic/recommended-extends',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@stylistic',
+  ],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@stylistic/multiline-ternary': 'off',
   },
 }
