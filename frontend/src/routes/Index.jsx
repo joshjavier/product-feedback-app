42e411c6cbc7d49d541f54757b51ff9f860fdc27
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-25 14:29:01 +0800

    Refactor Index route styles

--- frontend/src/routes/Index.jsx
+++ frontend/src/routes/Index.jsx
@@ -5,8 +5,8 @@ import Suggestions from '../components/Suggestions'
 
 const Index = () => {
   return (
-    <div className="container mx-auto px-4 max-w-[1110px] flex flex-col md:flex-row gap-[30px]">
-      <div className="sidebar md:max-w-[255px]">
+    <div className="container mx-auto px-4 xl:max-w-[1110px] flex flex-col md:flex-row gap-[30px]">
+      <div className="sidebar md:max-w-[255px] shrink-0">
         <div className="card card-body">
           <div>Frontend Mentor</div>
           <div>Feedback Board</div>
@@ -14,7 +14,7 @@ const Index = () => {
         <Filter />
         <RoadmapSummary />
       </div>
-      <div className="w-full">
+      <div className="grow">
         <Header />
         <Suggestions />
       </div>
