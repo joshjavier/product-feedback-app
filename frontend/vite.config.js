e3bf7279005ef9e382f6f67588dcf0b8d5685f86
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-31 19:16:42 +0800

    Add plugins for importing SVGs as React components

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'removeAttrs',
+              params: {
+                attrs: '(fill|stroke)',
+              },
+            },
+          ],
+        },
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
