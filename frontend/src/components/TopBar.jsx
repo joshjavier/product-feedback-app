af001a0af9edd7920a1319d25c5ae9694021eb5c
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-04-03 19:29:28 +0800

    Change hover bg color for primary button in topbar

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -7,7 +7,7 @@ const TopBar = () => {
     <div className="flex items-center gap-x-[38px] rounded-[10px] py-3.5 ps-6 pe-4 bg-neutral text-white">
       <SuggestionCount />
       <SortSelect />
-      <Link to="/feedback-new" className="btn btn-primary font-bold w-[158px] h-11 min-h-11 ms-auto">
+      <Link to="/feedback-new" className="btn btn-primary font-bold hover:bg-[#C75AF6] hover:border-[#C75AF6] w-[158px] h-11 min-h-11 ms-auto">
         <span aria-hidden="true">+</span> Add Feedback
       </Link>
     </div>
