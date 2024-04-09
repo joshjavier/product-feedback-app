3693a7e4786aebd0677ca2ad8f84dc376eaf0f63
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-04-09 17:34:05 +0800

    Update styles for edit feedback route

--- frontend/src/routes/FeedbackEdit.jsx
+++ frontend/src/routes/FeedbackEdit.jsx
@@ -5,26 +5,28 @@ import { categories, status } from '../features/ui'
 import Form from '../components/Form'
 import Field from '../components/Field'
 import BackButton from '../components/BackButton'
+import EditFeedbackIcon from '../assets/shared/icon-edit-feedback.svg?react'
 
 const FeedbackEdit = () => {
   const params = useParams()
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  // eslint-disable-next-line no-unused-vars
   const { id, upvotes, comments, ...feedback } = useSelector(state => selectFeedbackById(state, params.id))
 
   const onSubmit = (formData) => {
-    console.log(formData)
     dispatch(updateFeedback({ id, ...formData }))
     navigate(-1)
   }
 
   return (
-    <div className="mx-auto mt-[92px] px-6 box-content max-w-[540px]">
-      <div className="mb-10">
+    <div className="mx-auto mt-[34px] sm:mt-14 md:mt-[92px] px-6 box-content max-w-[540px] text-[13px] sm:text-sm">
+      <div className="mb-[55px] sm:mb-[68px]">
         <BackButton />
       </div>
-      <div className="card bg-white pt-[52px] px-[42px] pb-10 text-base-heading text-sm">
-        <h1 className="font-bold text-2xl tracking-[-0.33px] mb-[75px]">Editing &lsquo;{feedback.title}&rsquo;</h1>
+      <div className="card bg-white p-6 pt-11 sm:px-[42px] sm:pt-[52px] sm:pb-10">
+        <EditFeedbackIcon viewBox="0 0 40 40" className="absolute inset-0 translate-y-[-50%] ms-6 sm:ms-[42px] w-10 sm:w-14 h-auto" />
+        <h1 className="font-bold text-lg sm:text-2xl text-base-heading tracking-[-0.33px] mb-6 sm:mb-[75px]">Editing &lsquo;{feedback.title}&rsquo;</h1>
         <Form onSubmit={onSubmit} initialData={feedback} submitLabel="Save Changes">
           <Field
             name="title"
