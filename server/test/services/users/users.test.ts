e187e08f571e2bb4ad07e00ac033abb719a941b4
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-02-18 22:19:47 +0800

    Add tests to users service

--- server/test/services/users/users.test.ts
+++ server/test/services/users/users.test.ts
@@ -8,4 +8,51 @@ describe('users service', () => {
 
     assert.ok(service, 'Registered the service')
   })
+
+  it('finds all uers', async () => {
+    const users = await app.service('users').find({
+      paginate: false
+    })
+
+    assert.ok(Array.isArray(users))
+  })
+
+  it('creates a new user', async () => {
+    const user = await app.service('users').create({
+      username: 'newuser',
+      password: 'secret'
+    })
+
+    const [latestUser] = await app.service('users').find({
+      query: { $sort: { _id: -1 }, $limit: 1 },
+      paginate: false
+    })
+
+    assert.deepStrictEqual(user._id, latestUser._id)
+
+    await app.service('users').remove(user._id.toString())
+  })
+
+  it('deletes an existing user', async () => {
+    const user = await app.service('users').create({
+      username: 'deleteme',
+      password: 'secret'
+    })
+
+    const beforeResult = await app.service('users').find({
+      paginate: false,
+      pipeline: [{ $group: { _id: null, count: { $count: {} } } }]
+    })
+    const beforeCount = (beforeResult[0] as any).count
+
+    await app.service('users').remove(user._id.toString())
+
+    const afterResult = await app.service('users').find({
+      paginate: false,
+      pipeline: [{ $group: { _id: null, count: { $count: {} } } }]
+    })
+    const afterCount = (afterResult[0] as any).count
+
+    assert.strictEqual(beforeCount, afterCount + 1)
+  })
 })
