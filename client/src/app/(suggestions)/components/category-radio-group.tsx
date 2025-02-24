5689c29b35bb79615c70e898982fa28a54627177
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-02-24 17:54:03 +0800

    Style category links

--- client/src/app/(suggestions)/components/category-radio-group.tsx
+++ client/src/app/(suggestions)/components/category-radio-group.tsx
@@ -1,13 +1,39 @@
-export default function CategoryRadioGroup() {
+"use client";
+
+import Link from "next/link";
+import { Request } from "product-feedback";
+import { useState } from "react";
+
+function CategoryLink({
+  value,
+  children,
+}: {
+  value?: Request["category"];
+  children: React.ReactNode;
+}) {
+  const href = value ? `/?category=${value}` : "/";
   return (
-    <div>
+    <Link
+      href={href}
+      className="inline-flex justify-center items-center bg-zircon hover:bg-periwinkle text-royal-blue text-[13px] font-semibold px-4 min-w-12 min-h-[30] rounded-[10] transition-colors"
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default function CategoryRadioGroup() {
+  const [active, setActive] = useState(0);
+
+  return (
+    <div className="flex flex-wrap gap-3.5">
       <p className="sr-only">Categories</p>
-      <div>All</div>
-      <div>UI</div>
-      <div>UX</div>
-      <div>Enhancement</div>
-      <div>Feature</div>
-      <div>Bug</div>
+      <CategoryLink>All</CategoryLink>
+      <CategoryLink value="ui">UI</CategoryLink>
+      <CategoryLink value="ux">UX</CategoryLink>
+      <CategoryLink value="enhancement">Enhancement</CategoryLink>
+      <CategoryLink value="feature">Feature</CategoryLink>
+      <CategoryLink value="bug">Bug</CategoryLink>
     </div>
   );
 }
