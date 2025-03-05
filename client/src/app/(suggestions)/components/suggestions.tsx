881267f45a7e5db3663c60584ba7ef3c10a63df7
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-03-05 15:27:23 +0800

    Update styles for suggestions header

--- client/src/app/(suggestions)/components/suggestions.tsx
+++ client/src/app/(suggestions)/components/suggestions.tsx
@@ -2,6 +2,7 @@ import { sleep } from "@/lib/helpers";
 import SuggestionsList from "./suggestions-list";
 import client from "@/lib/client";
 import { Request } from "product-feedback";
+import IconSuggestions from "@/icons/icon-suggestions.svg";
 
 export default async function Suggestions({ category }: { category?: string }) {
   // Simulate delay to see the skeleton loader
@@ -16,8 +17,13 @@ export default async function Suggestions({ category }: { category?: string }) {
 
   return (
     <>
-      <div className="flex gap-[38px] items-center md:rounded-[10] bg-rhino text-white py-3.5 px-3 md:px-4 max-sm:-mx-6 max-md:-mx-10">
-        <div className="hidden sm:block">{total} Suggestions</div>
+      <div className="flex gap-[38px] items-center md:rounded-[10] bg-rhino text-white py-2 sm:py-3.5 pl-6 pr-6 md:pr-3 lg:pr-4 max-sm:-mx-6 max-md:-mx-10">
+        <div className="hidden sm:flex items-center gap-4 font-bold text-lg/[normal] tracking-[-0.25px]">
+          <IconSuggestions />
+          <span>
+            {total} {total === 1 ? "Suggestion" : "Suggestions"}
+          </span>
+        </div>
         <div>Sort by : Most Upvotes</div>
         <button className="ml-auto cursor-pointer bg-electric-violet hover:bg-[#C75AF6] text-zircon min-w-[134] sm:min-w-[158] min-h-10 sm:min-h-11 rounded-[10] font-bold text-[13px] sm:text-sm/[normal] transition-colors">
           + Add Feedback
